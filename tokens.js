6de1955ae53cd14e32f9dddc9d38e14dd9918e3a
Ojan Vafai <ojan@chromium.org>
2017-03-19 16:01:19 -0700

    Use memcache again.

--- tokens.js
+++ tokens.js
@@ -1,5 +1,5 @@
 var config = require('./config');
-// var memcache = require('./memcache').memcache;
+var memcache = require('./memcache').memcache;
 var Datastore = require('@google-cloud/datastore');
 
 const datastore = Datastore({
@@ -19,39 +19,36 @@ exports.save = (tokens) => {
 
   datastore.save(entity).then(
     () => {
-      console.log(`Saved tokens:`, tokens);
+      console.log("Wrote tokens to datastore.");
+      memcache.set(TOKEN_KEY, JSON.stringify(tokens), function(err, val) {
+        if (err)
+          console.log("Couldn't write tokens to memcache:", tokens, err);
+        else
+          console.log("Wrote tokens to memcache.");
+      }, 600);
     },
     (err) => {
       console.log(`Couldn't save tokens:`, tokens, err);
     }
   );
-
-  // memcache.set(TOKEN_KEY, JSON.stringify(tokens), function(err, val) {
-  //   if (err)
-  //     console.log("Couldn't write tokens to memcache");
-  //   else
-  //     console.log("Wrote tokens to memcache:", val);
-  // }, 600);
 }
 
 // TODO: Remove the copy-paste in thermostats.js.
 exports.get = (callback) => {
-  datastore.get(datastoreKey).then(
-    (entities) => {
-      callback(entities[0]);
-    },
-    (err) => {
-      console.log("Couldn't read tokens from datastore:", err);
-      callback();
+  memcache.get(TOKEN_KEY, function(err, val) {
+    if (err || !val) {
+      console.log("Couldn't get tokens from memcache. Getting from datastore:", err, val);
+      datastore.get(datastoreKey).then(
+        (entities) => {
+          callback(entities[0]);
+        },
+        (err) => {
+          console.log("Couldn't read tokens from datastore:", err);
+          callback();
+        }
+      );
+    } else {
+      return callback(JSON.parse(val));
     }
-  );
-
-  // memcache.get(TOKEN_KEY, function(err, val) {
-  //   if (err || !val) {
-  //     console.log("Couldn't get tokens from memcache:", err, val);
-  //     callback();
-  //   } else {
-  //     return callback(JSON.parse(val));
-  //   }
-  // }, 600);
+  }, 600);
 }
