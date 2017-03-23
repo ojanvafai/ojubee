b83b92631945978bb876d9ccacfcb765bc450820
Ojan Vafai <ojan@chromium.org>
2017-03-23 15:17:42 -0700

    Add a manifest.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.use(express.favicon(path.join(__dirname, 'public','img','favicon.ico')));
+  app.use(express.favicon(path.join(__dirname, 'public','img', 'icons', 'favicon.ico')));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
