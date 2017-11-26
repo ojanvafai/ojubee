a3b69850cb941c777580ea9e336113656fa00dc0
Ojan Vafai <ojan@chromium.org>
2017-11-25 19:21:09 -0800

    remove outdated files

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.appKey = 'WWiotjboBZKj9Bhf5KoZkxVwG5jZ8bau';
-exports.ecobeeHost = 'www.ecobee.com';
-exports.ecobeePort = '443';
-exports.isHTTPS = true;
-exports.scope = 'smartWrite';
-exports.GCLOUD_PROJECT = "oju-bee";
