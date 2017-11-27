282e7813a973d39d12db1bcfe6e1d46f5d303cef
Ojan Vafai <ojan@chromium.org>
2017-11-26 17:40:36 -0800

    Cleanup old files

--- test/test-config.js
+++
@@ -1,8 +0,0 @@
-exports.appKey = 'c87fjXJHnTu0xfRGBmgVlOhPy6zoRQ2H';
-exports.ecobeeHost = 'localhost';
-exports.ecobeePort = '8080';
-exports.scope = 'smartWrite';
-exports.isHTTPS = false;
-exports.test = {
-	thermostatId : '145561156354'
-}
