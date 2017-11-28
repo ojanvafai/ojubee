42c771bdf28640d431973a657a2e2fe3f8742823
Ojan Vafai <ojan@chromium.org>
2017-11-27 21:04:18 -0800

    Store tokens in a real database and make page transitions less buggy

--- static/tokens.js
+++ static/tokens.js
@@ -1,53 +1,57 @@
 const key = 'tokens';
 
+// TODO: Store this somewhere other than plain text.
+let apiKey = '1_feHI1criVLQfGzgGRvBlLTCf7WAUpz';
+let url = `https://api.mlab.com/api/1/databases/ojubee/collections/tokens/current?apiKey=${ apiKey }`;
+let headers = new Headers({
+  "Content-Type": "application/json",
+});
+
+// TODO: Change to throwing an error on !response.ok and having a catch handler so it
+// also catches things like being offline.
+
 export default class {
   static delete(callback) {
-    localStorage.removeItem(key);
-    callback();
-    // localStorage.delete(datastoreKey, function(err) {
-    //   if (err)
-    //     console.log("Couldn't delete tokens from datastore:", err);
-    //   else
-    //     console.log("Deleted tokens to datastore.");
-
-    //   callback();
-    // });
+    fetch(url, {
+      method: 'delete',
+      headers: headers,
+    }).then(function(response) {
+      if (!response.ok)
+        console.log("Couldn't delete tokens from datastore: ", tokens);
+      callback();
+    });
   };
 
   static save(tokens) {
+    fetch(url, {
+      method: 'put',
+      headers: headers,
+      body: JSON.stringify(tokens)
+    }).then(function(response) {
+      if (!response.ok) {
+        console.log("Couldn't write token to datastore: ", tokens);
+        return;
+      }
+      return response.text();
+    }).then(function(data) {
+      console.log("Wrote tokens to datastore: ", data);
+    });
+
     localStorage.setItem(key, JSON.stringify(tokens));
-
-    // const entity = {
-    //   key: datastoreKey,
-    //   data: tokens,
-    // };
-
-    // datastore.save(entity).then(
-    //   () => {
-    //     console.log("Wrote tokens to datastore:", tokens);
-    //   },
-    //   (err) => {
-    //     console.log(`Couldn't save tokens:`, tokens, err);
-    //   }
-    // );
   };
 
   static get(callback) {
-    let tokens = localStorage.getItem(key);
-    try {
-      callback(JSON.parse(tokens));
-    } catch (e) {
-      callback();
-    }
-
-  //   datastore.get(datastoreKey).then(
-  //     (entities) => {
-  //       callback(entities[0]);
-  //     },
-  //     (err) => {
-  //       console.log("Couldn't read tokens from datastore:", err);
-  //       callback();
-  //     }
-  //   );
+    fetch(url, {
+      method: 'get',
+      headers: headers,
+    }).then(function(response) {
+      if (!response.ok) {
+        callback();
+        return;
+      }
+      return response.json();
+    }).then(function(tokens) {
+      callback(tokens);
+    });
   };
 }
