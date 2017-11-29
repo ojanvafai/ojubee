c963d92d4ebef1bc72279197ac8ed5e4c5778652
Ojan Vafai <ojan@chromium.org>
2017-11-28 22:15:52 -0800

    Fix backgwards boolean for syncing and delete dead code

--- public/js/thermostats.js
+++ public/js/thermostats.js
@@ -1,53 +1,7 @@
-var g_thermostats;
-var g_pageSpecificNavCallback;
-
 window.addEventListener('online', () => { window.location.reload(); });
 window.addEventListener('offline', () => {
   document.body.classList.add('offline');
 });
-
-window.addEventListener('DOMContentLoaded', () => {
-  fetch('/thermostats/json')
-    .then((response) => {
-      if (!response.ok) {
-        alert("Couldn't load thermostat list. Please reload.");
-        return;
-      }
-
-      response.json()
-        .then((json) => {
-          if (json.error) {
-            window.location = json.redirectUrl + window.location.pathname;
-            return;
-          }
-
-          g_thermostats = json.thermostats;
-          generateNavBar();
-          if (g_pageSpecificNavCallback)
-            g_pageSpecificNavCallback();
-        });
-    })
-    .catch(() => {
-      alert("Couldn't load thermostat list. Please reload.");
-    });
-});
-
-function appendLink(container, href, text) {
-  var a = document.createElement('a');
-  a.href = href;
-  a.textContent = text;
-  if (a.pathname == window.location.pathname)
-    a.className = 'current';
-  container.appendChild(a);
-}
-
-function generateNavBar() {
-  var navBar = document.getElementById('nav');
-  appendLink(navBar, "/", "Ojubee");
-  g_thermostats.forEach((thermostat) => {
-    appendLink(navBar, `/thermostats/${thermostat.id}`, thermostat.name);
-  });
-}
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('../service-worker.js').then(
